22e800110d8a938cec3473ea2675900f9e64b70c
Upendra Khura <upendrakhura0@gmail.com>
2026-03-14 19:32:37 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
@@ -8,5 +6,4 @@ public class Main {
                 System.out.println("i = " + i);
         }
     }
-    // Hello
 }
