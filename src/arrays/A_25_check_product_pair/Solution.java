9a68cd16df23d77ec67749656b9f890c65596a9b
Upendra Khura <upendrakhura0@gmail.com>
2026-05-21 20:40:18 +0530

    added question to A_25

--- src/arrays/A_25_check_product_pair/Solution.java
+++ src/arrays/A_25_check_product_pair/Solution.java
@@ -1,6 +1,31 @@
 package arrays.A_25_check_product_pair;
 
 import java.util.HashSet;
+/*
+        Product Pair
+        Difficulty: Medium
+        Given an integer array arr[] and an integer target, determine whether there exists a pair of elements
+         in the array whose product is equal to target.
+
+        Return true if such a pair exists; otherwise, return false.
+
+        Examples:
+
+        Input: arr[] = [10, 20, 9, 40], target = 400
+        Output: true
+        Explanation: As 10 * 40 = 400, the answer is true.
+        Input: arr[] = [-10, 20, 9, -40], target = 30
+        Output: false
+        Explanation: No pair exists with product 30.
+        Input: arr[] = [-10, 0, 9, -40], target = 0
+        Output: true
+        Explanation: As -10 * 0 = 0, the answer is true.
+
+        Constraints:
+        2 ≤ arr.size ≤ 105
+        -108 ≤ arr[i] ≤ 108
+        -1018 ≤ target ≤ 1018
+ */
 
 public class Solution {
     public static void main(String[] args) {
