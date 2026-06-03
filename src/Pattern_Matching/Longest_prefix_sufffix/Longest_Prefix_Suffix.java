171a029de6b99d4fc0d456e95c27808bab8e2523
Upendra Khura <upendrakhura0@gmail.com>
2026-06-03 22:34:23 +0530

    Solved Longest_Prefix_Suffix

---
+++ src/Pattern_Matching/Longest_prefix_sufffix/Longest_Prefix_Suffix.java
@@ -0,0 +1,57 @@
+package Pattern_Matching.Longest_prefix_sufffix;
+/*
+        Longest Prefix Suffix
+        Difficulty: HardAccuracy: 27.91%Submissions: 169K+Points: 8
+        Given a string s, of lowercase english alphabets, find the length of the longest proper prefix which is also a suffix.
+        Note: Prefix and suffix can be overlapping but they should not be equal to the entire string.
+
+        Examples :
+
+        Input: s = "abab"
+        Output: 2
+        Explanation: The string "ab" is the longest prefix and suffix.
+
+        Input: s = "aabcdaabc"
+        Output: 4
+        Explanation: The string "aabc" is the longest prefix and suffix.
+
+        Input: s = "aaaa"
+        Output: 3
+        Explanation: "aaa" is the longest prefix and suffix.
+
+        Constraints:
+        1 ≤ s.size() ≤ 106
+        s contains only lowercase English alphabets.
+*/
+
+public class Longest_Prefix_Suffix {
+    public static void main(String[] args) {
+
+    }
+    static int getLPSLength(String s) {
+        // code here
+        int n = s.length();
+
+        int[] lps = new int[n];
+        int pre = 0;
+        int suf = 1;
+
+        while(suf < n){
+            if(s.charAt(pre) == s.charAt(suf)){
+                lps[suf] = pre + 1;
+                pre++;
+                suf++;
+            }
+            else{
+                if(pre == 0){
+                    lps[suf] = 0;
+                    suf++;
+                }
+                else{
+                    pre = lps[pre - 1];
+                }
+            }
+        }
+        return lps[n - 1];
+    }
+}
