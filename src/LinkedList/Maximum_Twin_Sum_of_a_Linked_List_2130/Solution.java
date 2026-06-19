e7603191b623d0a6ad75da49aeac1afeb8091391
Upendra Khura <upendrakhura0@gmail.com>
2026-06-19 21:42:08 +0530

    Solved Leetcode 2130

--- src/LinkedList/Maximum_Twin_Sum_of_a_Linked_List_2130/Solution.java
+++ src/LinkedList/Maximum_Twin_Sum_of_a_Linked_List_2130/Solution.java
@@ -56,7 +56,7 @@ class ListNode {
       int val;
       ListNode next;
       ListNode() {}
-      ListNode(int val) { this.val = val; }
+      ListNode(int val) { this.val = val;   }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
   }
 public class Solution {
