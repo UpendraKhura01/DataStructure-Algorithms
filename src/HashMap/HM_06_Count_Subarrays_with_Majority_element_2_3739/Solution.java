34e1ff3d7840a9badb84958e851e6ac50d2fb2db
Upendra Khura <upendrakhura0@gmail.com>
2026-06-26 22:30:00 +0530

    Solved Leetcode 3739

---
+++ src/HashMap/HM_06_Count_Subarrays_with_Majority_element_2_3739/Solution.java
@@ -0,0 +1,91 @@
+package HashMap.HM_06_Count_Subarrays_with_Majority_element_2_3739;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+        3739. Count Subarrays With Majority Element II
+
+        Hard
+
+        You are given an integer array nums and an integer target.
+
+        Return the number of subarrays of nums in which target is the majority element.
+
+        The majority element of a subarray is the element that appears strictly more than half of the times in that subarray.
+
+
+
+        Example 1:
+
+        Input: nums = [1,2,2,3], target = 2
+
+        Output: 5
+
+        Explanation:
+
+        Valid subarrays with target = 2 as the majority element:
+
+        nums[1..1] = [2]
+        nums[2..2] = [2]
+        nums[1..2] = [2,2]
+        nums[0..2] = [1,2,2]
+        nums[1..3] = [2,2,3]
+        So there are 5 such subarrays.
+
+        Example 2:
+
+        Input: nums = [1,1,1,1], target = 1
+
+        Output: 10
+
+        Explanation:
+
+       All 10 subarrays have 1 as the majority element.
+
+        Example 3:
+
+        Input: nums = [1,2,3], target = 4
+
+        Output: 0
+
+        Explanation:
+
+        target = 4 does not appear in nums at all. Therefore, there cannot be any subarray where 4 is the majority element.
+        Hence the answer is 0.
+
+
+
+        Constraints:
+
+        1 <= nums.length <= 10^5
+        1 <= nums[i] <= 10^9
+        1 <= target <= 10^9
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    long countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        long cur = 0;
+        long ans = 0;
+        long valid = 0;
+        Map<Long, Long> mp = new HashMap<>();
+        mp.put(0L, 1L);
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] == target){
+                valid += mp.getOrDefault(cur, 0L);
+                cur++;
+            }
+            else{
+                cur--;
+                valid -= mp.getOrDefault(cur, 0L);
+            }
+            mp.put(cur, mp.getOrDefault(cur, 0L) + 1L);
+            ans += valid;
+        }
+        return ans;
+    }
+}
